5e8cef821db830e9503c0b52e5f784cd829d3105
Alex O <amo25@vt.edu>
2019-03-02 16:22:37 -0500

    Added the FPGA configuration for the 50 Hz PWM scripts, as well as the 50 Hz PWM scripts

---
+++ PWM_DC10.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Prereq: Run init script to turn on PWM0 and Global PWM
+#
+#Set frequency registers (0x18 and 0x17) to 0x30D4
+spi_xfer -b 1 -c 1 -d 3098 -w 2
+spi_xfer -b 1 -c 1 -d D497 -w 2
+#
+#Set duty registers (0x32 and 0x31) to 0x09F6
+spi_xfer -b 1 -c 1 -d 09B2 -w 2
+spi_xfer -b 1 -c 1 -d F6B1 -w 2
