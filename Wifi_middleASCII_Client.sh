ecf66c663fa9fb00ac086a2637c4364eef7d76f6
Alex Orlov <31498402+amo25@users.noreply.github.com>
2020-04-09 03:06:05 -0400

    Delete Wifi_middleASCII_Client.sh

--- Wifi_middleASCII_Client.sh
+++
@@ -1,6 +0,0 @@
-nmcli c down hotspot
-nmcli c modify hotspot connection.autoconnect no
-# scan wifi networks
-# nmcli dev wifi
-# connect
-nmcli dev wifi connect ***REMOVED*** password ***REMOVED***
